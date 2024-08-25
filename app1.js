5572ce301b7f23cd416efdb2f5ab702146a6726e
LuisIvan2021 <ivan_dokaknel@hotmail.com>
2024-08-25 13:12:52 -0600

    se agrego los archivos js y html

---
+++ app1.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Cual es tu nombre?");
+let edad = prompt("Que edad tienes?")
+let peso = prompt("Cuanto pesas(KG)?")
+console.log("Hola "+ nombre);
+console.log("tu edad es: "+ edad);
+console.log("tu peso es: "+ peso);
